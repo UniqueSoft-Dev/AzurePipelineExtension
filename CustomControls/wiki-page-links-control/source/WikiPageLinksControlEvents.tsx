249963dc134df87fca10f0c5694dd0ce5ccf32b2
Attila Tóth <me_bf-109@freemail.com>
2024-05-26 10:13:14 +0300

    Reset custom CustomControls

--- CustomControls/wiki-page-links-control/source/WikiPageLinksControlEvents.tsx
+++
@@ -1,8 +0,0 @@
-import { initializeIcons } from "office-ui-fabric-react/lib/Icons";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-//import { getClient } from "TFS/WorkItemTracking/RestClient";
-//import { WorkItemFormService } from "TFS/WorkItemTracking/Services";
-
-//import { getSuggestedValues } from "./getSuggestedValues";
-//import { MultiValueControl } from "./MultiValueControl";
