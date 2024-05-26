d16692b725a4cf1bcbdfc5dac648361672a96d54
Attila Tóth <me_bf-109@freemail.com>
2024-05-26 10:08:22 +0300

    Reorganize project

---
+++ CustomControls/wiki-page-links-control/source/WikiPageLinksControlEvents.tsx
@@ -0,0 +1,8 @@
+import { initializeIcons } from "office-ui-fabric-react/lib/Icons";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+//import { getClient } from "TFS/WorkItemTracking/RestClient";
+//import { WorkItemFormService } from "TFS/WorkItemTracking/Services";
+
+//import { getSuggestedValues } from "./getSuggestedValues";
+//import { MultiValueControl } from "./MultiValueControl";
